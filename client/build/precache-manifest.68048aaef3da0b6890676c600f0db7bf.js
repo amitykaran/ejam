1356c6169f898103a342ce8ca1d6fb29179574b6
amit <amit.yadav@venuemonk.com>
2020-06-14 20:33:12 +0530

    final build

---
+++ client/build/precache-manifest.68048aaef3da0b6890676c600f0db7bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5646d08d15c0d4c03951b50da1854828",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3664721b291a2b2f11f2",
+    "url": "/static/css/main.6bafc950.chunk.css"
+  },
+  {
+    "revision": "ebd0cb6db95cabd3cf04",
+    "url": "/static/js/2.61515b9c.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.61515b9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3664721b291a2b2f11f2",
+    "url": "/static/js/main.fcbd53b0.chunk.js"
+  },
+  {
+    "revision": "e661affb54845a79021d",
+    "url": "/static/js/runtime-main.d16d2295.js"
+  }
+]);
