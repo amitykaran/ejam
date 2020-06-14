baa2599da1cb57ed3b105d3a07da96c2c24690ad
amit <amit.yadav@venuemonk.com>
2020-06-14 20:04:18 +0530

    minor chnages

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 
 module.exports =  {
     stage: 'local',
-    MONGO_URL: 'mongodb://localhost:27017/ejam',
+    MONGO_URL: 'mongodb+srv://ejamuser:xUtz0nzty8gCjy5d@cluster0-w4rdf.gcp.mongodb.net/deployments?retryWrites=true&w=majority',
     PORT: 8080
 }
