00cf408c0a1565daef643f44dbdb73b460acf0da
amit <amit.yadav@venuemonk.com>
2020-06-14 23:24:47 +0530

    minor chnaghes

---
+++ client/build/precache-manifest.b3b087942777715190a23c89130f0075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6b0341c981029796fcbb0e007274dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3ba048bd5a6f44a742a",
+    "url": "/static/css/main.07fd1464.chunk.css"
+  },
+  {
+    "revision": "ebd0cb6db95cabd3cf04",
+    "url": "/static/js/2.61515b9c.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.61515b9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3ba048bd5a6f44a742a",
+    "url": "/static/js/main.dd61853d.chunk.js"
+  },
+  {
+    "revision": "e661affb54845a79021d",
+    "url": "/static/js/runtime-main.d16d2295.js"
+  }
+]);
